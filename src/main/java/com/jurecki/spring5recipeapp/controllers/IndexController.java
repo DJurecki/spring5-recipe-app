f8978b390d343e61b987ecab0fe1f5aa150677b3
DJurecki <dejwux1@gmail.com>
2020-02-23 18:00:17 +0100

    Added new models.

---
+++ src/main/java/com/jurecki/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.jurecki.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = {"", "/", "/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
