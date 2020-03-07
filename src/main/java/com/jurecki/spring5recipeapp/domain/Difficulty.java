cc7cd3db1fd81681e0cbb6f3ce989dd0275e7bde
DJurecki <dejwux1@gmail.com>
2020-03-07 20:36:22 +0100

    Added a lot of functionality. Updated views, controllers, domains, services, adding image to db from the web page. Also added Junit tests.

--- src/main/java/com/jurecki/spring5recipeapp/domain/Difficulty.java
+++ src/main/java/com/jurecki/spring5recipeapp/domain/Difficulty.java
@@ -2,5 +2,5 @@ package com.jurecki.spring5recipeapp.domain;
 
 public enum Difficulty {
 
-    EASY, MODERATE, HARD
+    EASY, MODERATE, HARD, KIND_OF_HARD
 }
---
+++ src/main/java/com/jurecki/spring5recipeapp/services/UnitOfMeasureService.java
@@ -0,0 +1,10 @@
+package com.jurecki.spring5recipeapp.services;
+
+import com.jurecki.spring5recipeapp.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+public interface UnitOfMeasureService {
+
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
